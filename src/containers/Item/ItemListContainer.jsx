34473d828a0c04a520971683597f6bd65ee605c9
Matias E. Navarrete <matias.navarreteutn@gmail.com>
2022-01-02 10:13:22 -0300

    code refactorized

--- src/containers/Item/ItemListContainer.jsx
+++ src/containers/Item/ItemListContainer.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ItemListContainer.css';
+import productData from '../../data/products.json'
 import { ItemList } from '../../components/ItemList/ItemList';
+import { Loading } from '../../components/Loading/Loading';
 
 export const ItemListContainer = ({ greeting }) => {
-   
-    
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (loading) {
+            new Promise((res, rej) => {
+                setTimeout(() => {
+                    const productsArray = productData.length > 0 ? productData : []
+                    setLoading(!loading);
+                    res(productsArray)
+                }, 2000)
+            }).then((res) => {
+                setProducts(res);
+            })
+        }
+    }, [loading])
+
+
+
     return (
         <div className="container">
             <div className="greeting">
                 <h1>{greeting}</h1>
             </div>
             <div>
-                {
-                    < ItemList />
-                }
+                {loading ? <Loading /> : <ItemList products={products} />}
             </div>
         </div>
     )
